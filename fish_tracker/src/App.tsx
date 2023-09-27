639839ce384e78275953c963398b73f426e62a5f
Caleb Norris <paintballerz15@gmail.com>
2023-09-27 16:37:34 -0600

    Setup react gmaps api library

--- fish_tracker/src/App.tsx
+++ fish_tracker/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Map from "./components/Map";
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <h1>Fish Tracker</h1>
-
+      <Map />
     </div>
   );
 }
--- fish_tracker/src/components/Map.tsx
+++ fish_tracker/src/components/Map.tsx
@@ -1,19 +1,82 @@
-import React from 'react';
-import './Map.css';
+import React from "react";
+import "../css/Map.css";
 
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: "400px",
+  height: "400px",
 };
 
+interface Mark {
+  lat: number;
+  lng: number;
+}
+
 function Map(): JSX.Element {
-    return (
-        <div className="map_component">
-            <h1>Map</h1>
-        </div>
-    );
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "",
+  });
+  const [map, setMap] = React.useState(null);
+  const [markers, setMarkers] = React.useState<Mark[]>([]); // pass empty array as initial state
+
+  const [center, setCenter] = React.useState({ lat: 0, lng: 0 });
+
+  const onLoad = React.useCallback(function callback(map: any) {
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map: any) {
+    setMap(null);
+  }, []);
+
+  React.useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCenter({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+          setMarkers([
+            ...markers,
+            {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            } as Mark, // add type assertion to fix type error
+          ]);
+        },
+        () => {
+          console.log("Error: The Geolocation service failed.");
+        }
+      );
+    } else {
+      console.log("Error: Your browser doesn't support geolocation.");
+    }
+  }, []);
+
+  console.log(markers);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+
+      {markers.map((mark) => {
+        return <MarkerF position={{ lat: mark.lat, lng: mark.lng }} />;
+      })}
+    </GoogleMap>
+  ) : (
+    <div className="map_component">
+      <h1>Map</h1>
+    </div>
+  );
 }
 
 export default Map;
